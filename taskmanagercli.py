26d8f2d2c61460bc33f8ce88d1002fbc18fd7a40
2andahalfNeumann <thomasneumanndk@gmail.com>
2025-03-25 14:01:44 +0100

    Functionality updates
    Major changes for task_list():
    Added: Option to either view all, view in-progress or done + option to opt out.
    
    Changed the task_update_status() func:
    Instead of having to write the status "in-progress" or "done", it checks itself that the
    status is, and switches it.
    
    Added a load_tasks() func, for loading the json file to decrease amount of redundancy.
    
    Trimmed tasks.json file.
    
    Updated todo-list.

--- taskmanagercli.py
+++ taskmanagercli.py
@@ -6,14 +6,13 @@ import colourOutput as co
 #Add a task function
 def task_add():
     # Load Tasks
-    with open("tasks.json", "r") as f:
-        tasks = json.load(f)
+    tasks = load_tasks()
 
     # id: A unique identifier for the task
     task_id = len(tasks) + 1
     
     # description: A short description of the task
-    task_desc = input("Add description: ")
+    task_desc = input("\nAdd description: ")
     
     # status: The status of the task (todo, in-progress, done)
     task_status = "in-progress"
@@ -38,14 +37,16 @@ def task_add():
     with open(r"tasks.json", "w") as f:
         json.dump(tasks, f, indent=4)
         
-    print(f"Task added successfully (ID: {task_id})")
-
+    print(f"\nTask added successfully (ID: {task_id})\n")
+    print("\n" + "-" * 30 + "\n")
 
 #Update desc
 def task_update_desc():
     # Load Tasks
-    with open("tasks.json", "r") as f:
-        tasks = json.load(f)
+    tasks = load_tasks()
+    if not tasks:
+        print("No tasks available")
+        return
     
     print("-" * 30 + "\n")
     print("\nAvailable Tasks:\n")
@@ -54,18 +55,22 @@ def task_update_desc():
         print(f"ID: {task['task_id']} | Task: {task['task_desc']} | Status: {status_color}{task['task_status']}{co.RESET} | Date created: {task['task_createdAt']} | Date Updated: {task['task_updatedAt']}")
         print("-" * 30 + "\n")
     
-    update_id = int(input("Enter task ID to update: "))
+    update_id = int(input("\nEnter task ID to update: "))
 
     # Find the task by ID
-    update_id_data = next((task for task in tasks if task["task_id"] == update_id), None)
+    update_id_data = next((task for task in tasks if task['task_id'] == update_id), None)
 
     if update_id_data:
-        new_desc = input("Enter new description: ")
+        new_desc = input("\nEnter new description: ")
         update_id_data["task_desc"] = new_desc
 
         # Updates task_updatedAt
         update_id_data["task_updatedAt"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
+        print(f"\nDescription for ID: {update_id_data['task_id']} has been changed to:\n{new_desc}")
+        print("\n" + "-" * 30 + "\n")
+
+
     # Saves task desc + updatedAt to JSON file
     with open("tasks.json", "w") as f:
         json.dump(tasks,  f,  indent=4)
@@ -73,14 +78,12 @@ def task_update_desc():
 
 #Delete function
 def task_delete():
-    """
-    Added error handling
-    """
-    
     try:
         # Load Tasks
-        with open("tasks.json", "r") as f:
-            tasks = json.load(f)
+        tasks = load_tasks()
+        if not tasks:
+            print("No tasks to display")
+            return
 
         print("-" * 30 + "\n")
         print("\nAvailable Tasks:\n")
@@ -92,22 +95,22 @@ def task_delete():
         # User input
         while True:
             try:
-                delete_id = int(input("\nEnter task ID to delete: "))
-                break
+                task_delete_id = int(input("\nEnter task ID to delete (Press 0 to opt out): "))
             except ValueError:
-                print("Provided number is not valid \nPlease try again...")
-
-        #Filtering tasks
-        new_tasks = [task for task in tasks if task["task_id"] != delete_id]
-
-        with open("tasks.json", "w") as f:
-            json.dump(new_tasks, f, indent=4)
-        
-        print(f"Successfully deleted task  {delete_id}")
+                print("\nProvided number is not valid \nPlease try again...")
+                
+            if task_delete_id == 0:
+                break
+            else:
+                if task_delete_id:
+                    #Filtering tasks
+                    new_tasks = [task for task in tasks if task["task_id"] != task_delete_id]
+                    with open("tasks.json", "w") as f:
+                        json.dump(new_tasks, f, indent=4)
+                    
+                    print(f"\nSuccessfully deleted task {task_delete_id}")
+                    break
     
-        """
-        Currently added "errno" I stumbled across whilst testing.
-        """
     except FileNotFoundError:
         print("No tasks file found!")
     except json.JSONDecodeError:
@@ -118,15 +121,49 @@ def task_delete():
 def task_list():
     try:
     # Load Tasks
-        with open("tasks.json", "r") as f:
-            tasks = json.load(f)
+        tasks = load_tasks()
+        if not tasks:
+            print("No tasks available")
+            return
 
-        print("-" * 30 + "\n")
-        print("\nAvailable Tasks:\n")
-        for task in tasks:
-            status_color = co.ORANGE if task['task_status'] == "in-progress" else co.GREEN
-            print(f"ID: {task['task_id']} | Task: {task['task_desc']} | Status: {status_color}{task['task_status']}{co.RESET} | Date created: {task['task_createdAt']} | Date Updated: {task['task_updatedAt']}")
+        while True:
             print("-" * 30 + "\n")
+            print("Select filter:")
+            print("View all tasks: 1")
+            print("View tasks in-progress: 2")
+            print("View tasks done: 3")
+            print("Opt out: 4")
+            
+            task_list_choice = int(input("\nEnter value for list: "))
+
+            if task_list_choice == 1:
+                print("-" * 30 + "\n")
+                print("\nList of all tasks:\n")
+                for task in tasks:
+                    status_color = co.ORANGE if task['task_status'] == "in-progress" else co.GREEN
+                    print(f"ID: {task['task_id']} | Task: {task['task_desc']} | Status: {status_color}{task['task_status']}{co.RESET} | Date created: {task['task_createdAt']} | Date Updated: {task['task_updatedAt']}")
+                    print("-" * 30 + "\n")
+            elif task_list_choice == 2:
+                in_progress_tasks = [task for task in tasks if task['task_status'] == 'in-progress']
+                print("-" * 30 + "\n")
+                print("\nList of in-progress tasks:\n")
+                for task in in_progress_tasks:
+                    status_color = co.ORANGE
+                    print(f"ID: {task['task_id']} | Task: {task['task_desc']} | Status: {status_color}{task['task_status']}{co.RESET} | Date created: {task['task_createdAt']} | Date Updated: {task['task_updatedAt']}")
+                    print("-" * 30 + "\n")
+            elif task_list_choice == 3:
+                done_tasks = [task for task in tasks if task['task_status'] == 'done']
+                print("-" * 30 + "\n")
+                print("\nList of done tasks:\n")
+                for task in done_tasks:
+                    status_color = co.GREEN
+                    print(f"ID: {task['task_id']} | Task: {task['task_desc']} | Status: {status_color}{task['task_status']}{co.RESET} | Date created: {task['task_createdAt']} | Date Updated: {task['task_updatedAt']}")
+                    print("-" * 30 + "\n")
+            if task_list_choice == 4:
+                print("Opting out")
+                break
+            else:
+                print("Invalid number")
 
     except FileNotFoundError:
         print("No tasks file found!")
@@ -143,37 +180,35 @@ def task_update_status():
         
         print("-" * 30 + "\n")
         print("\nAvailable Tasks:\n")
+        print("\n" + "-" * 30 + "\n")
         for task in tasks:
             status_color = co.ORANGE if task['task_status'] == "in-progress" else co.GREEN
             print(f"ID: {task['task_id']} | Task: {task['task_desc']} | Status: {status_color}{task['task_status']}{co.RESET} | Date created: {task['task_createdAt']} | Date Updated: {task['task_updatedAt']}")
-            print("-" * 30 + "\n")
+            print("\n" + "-" * 30 + "\n")
 
         while True:
             try:
                 task_id_update = int(input("\nEnter task ID to update: "))
                 task_to_update = next((task for task in tasks if task['task_id'] == task_id_update), None)
-
                 if task_to_update is None:
-                    print("Task not found!")
+                    print("\nTask not found!")
                     continue
-
-                while True:
-                    new_status = input("Enter new status ('in-progress' or 'done'): ")
-                    if new_status in ['in-progress', 'done']:
-                        break
-                    print("Invalid status! Please enter 'in-progress' or 'done'")
-
-                # Update task status
-                task_to_update['task_status'] = new_status
+                
+                # Instead of having user writing either in-progress or done
+                # The codes updates it itself. Nice
+                if task_to_update['task_status'] == 'in-progress':
+                    task_to_update['task_status'] = 'done'
+                else:
+                    task_to_update['task_status'] = 'in-progress'
 
                 with open("tasks.json", "w") as f:
                     json.dump(tasks, f, indent=4)
 
-                print(f"\nTask {task_id_update} has been updated")
+                print(f"\nStatus for ID {task_id_update} has been updated to {task_to_update['task_status']}")
                 break
 
             except ValueError as e:
-                print(f"Invalid input: {e}\n")
+                print(f"\nInvalid input: {e}\n")
 
     except FileNotFoundError:
         print("No tasks file found!")
@@ -181,3 +216,14 @@ def task_update_status():
         print("Invalid JSON format in tasks file!")
     except Exception as e:
         print(f"An error occured: {e}")
+
+def load_tasks():
+    try:
+        with open("tasks.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("No tasks file found")
+        return []
+    except json.JSONDecodeError:
+        print("Invalid JSON formart in tasks file")
+        return []
